d0264039ed195fa63287f3042e54f05a2925eef4
Diego Arce <Diego@arce.cr>
2017-02-20 17:38:18 -0600

    Index.js
    Expose the devApi in index.

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+/*!
+ * Author Diego Arce E.
+ * @version 1.0
+ * Copyright(c) 2016 ArcezD
+ */
+
+const devApi = require('./dev/api.js');
+
+/**
+ * Expose internals.
+ */
+ exports.devApi = devApi.myHandler;
